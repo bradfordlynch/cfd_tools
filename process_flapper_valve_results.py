ba465e3ec2ac1f6111ac0e754c1f6ab9a1ef398c
Bradford Lynch <blynch41@gmail.com>
2014-08-21 16:01:57 -0500

    Example file to process CFD on flapper valves

---
+++ process_flapper_valve_results.py
@@ -0,0 +1,8 @@
+################################################################################
+
+#Script to process CFD on flapper valve designs
+
+#Stadia42, Bradford Lynch, 2014, Chicago, IL
+
+################################################################################
+import automated_ansys_post_processing
